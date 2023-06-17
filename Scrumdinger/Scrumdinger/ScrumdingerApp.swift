a7b006a107b76a5e4606e59e9bce1f16999b8c79
Juan <jmoreno@novopayment.com>
2023-06-17 10:38:19 -0500

    Create a project

---
+++ Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Juan Manuel Moreno on 17/06/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
